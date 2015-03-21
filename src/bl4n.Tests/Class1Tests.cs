c0707f23e8cdb303f084d431ca0a07c342be9b20
t.ashula <office@ashula.info>
2015-03-21 19:03:55 +0900

    fixes #59

---
+++ src/bl4n.Tests/Class1Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using bl4n;
+using Xunit;
+namespace bl4n.Tests
+{
+    public class Class1Tests
+    {
+        [Fact()]
+        public void Class1Test()
+        {
+            Assert.True(false, "not implemented yet");
+        }
+    }
+}
---
+++ src/bl4ncui.Tests/ProgramTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using bl4ncui;
+using Xunit;
+
+namespace bl4ncui.Tests
+{
+    public class ProgramTests
+    {
+        [Fact()]
+        public void MainTest()
+        {
+            Assert.True(false, "not implemented yet");
+        }
+    }
+}
