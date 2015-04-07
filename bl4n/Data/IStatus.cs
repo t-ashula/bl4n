e14a60040c0eb09065424885ecfbe54132a8f14f
t-ashula <t.ashula+github@gmail.com>
2015-04-07 20:57:28 +0900

    fix file header comment

--- bl4n/Data/IStatus.cs
+++ bl4n/Data/IStatus.cs
@@ -1,5 +1,5 @@
 // --------------------------------------------------------------------------------------------------------------------
-// <copyright file="IIssue.cs">
+// <copyright file="IStatus.cs">
 //   bl4n - Backlog.jp API Client library
 //   this file is part of bl4n, license under MIT license. http://t-ashula.mit-license.org/2015/
 // </copyright>
